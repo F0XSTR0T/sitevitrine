66e741ecb4015fe03016e47cd9dcb60c6a68c4fc
Julien2L <julien.lehoussine-labatut@epitech.eu>
2023-11-03 17:41:47 +0100

    setuo site vitrine de base

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div className="logo">
+          <img src="./logo192.png" alt="Logo Société" /> 
+          <h3>Nom de la société</h3> 
+        </div>
+    );
+};
+
+export default Logo;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const NotFound = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>pages/NotFound.js</h1>
+    </div>
+  );
+};
+
+export default NotFound;
